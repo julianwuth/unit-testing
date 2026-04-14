87127f31b531a5a74eb3b48edb8fa374ad63b1a5
jvli4n <julian.wuth@yahoo.de>
2026-04-14 19:36:18 +0200

    Simple function and unit tests

---
+++ R/col_summary.R
@@ -0,0 +1,32 @@
+#' Column summaries
+#'
+#' @param df A data.frame, array or matrix.
+#' @param fun The summary to be applied. One of "mean", "sd", "min", "max".
+#'
+#' @returns Named vector with per column summaries.
+#' @export
+#'
+col_summary <-  function(df, fun = c("mean", "sd", "min", "max")) {
+  if (!any(is.data.frame(df), is.matrix(df), is.array(df)))
+    stop("df needs to be one of data.frame, array or matrix.")
+
+  if (anyNA(df))
+    stop("Please remove NAs from the data before using this function.")
+
+  summary_fun <- match.arg(fun)
+
+  if (summary_fun == "sd") {
+    unique_elements <- apply(df, 2, get_unique)
+
+    if(any(unique_elements) < 2)
+      stop("SD cannot be computed because one column has less than two unique elements.")
+  }
+
+  res <- apply(df, 2, match.fun(summary_fun))
+
+  return(res)
+}
+
+get_unique <- function(col) {
+  return(length(unique(col)))
+}
---
+++ tests/testthat/test-col_summary.R
@@ -0,0 +1,28 @@
+test_that("col mean works", {
+  df <- data.frame(a = 1:3, b = 4:6, c = 6:8)
+  expect_equal(col_summary(df, "mean"), c("a" = 2, "b" = 5, "c" = 7))
+})
+
+test_that("col min works", {
+  df <- data.frame(a = 1:3, b = 4:6, c = 6:8)
+  expect_equal(col_summary(df, "min"), c("a" = 1, "b" = 4, "c" = 6))
+})
+
+# TODO Insert test for col max here
+
+
+# Tests for errors
+test_that("invalid df input errors", {
+  expect_error(col_summary(1), regexp = "df needs to be")
+  expect_error(col_summary(c(1, 2)))
+  expect_error(col_summary(Inf))
+  expect_error(col_summary(NA))
+  expect_error(col_summary(NULL))
+  expect_error(col_summary(data.frame(a = c(1, 2), b = c(NA, 30))), regexp = "Please remove NAs")
+})
+
+
+test_that("sd not computed if any column has one unique value only", {
+  df <- data.frame(c(1, 2), c(1, 1))
+  expect_error(col_summary(df, "sd"))
+})
